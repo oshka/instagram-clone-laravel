ac043dfe20a83ad0e707fb058c894e1c6f93b690
Olha Babchenko <olka.babchenko@gmail.com>
2021-02-24 14:02:45 +0200

    [ADD] creating auth, adding username to user table

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://cdn.changelog.com/uploads/icons/news_sources/Qo/icon_small.png?v=63680075631" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freecodecamp</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23K</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org </div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="">www.freecodecamp.org</a></div>
+
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.fnlv2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c3.0.822.822a/s640x640/153220043_178088813820747_7489645115129348672_n.jpg?_nc_ht=instagram.fnlv2-1.fna.fbcdn.net&amp;_nc_cat=103&amp;_nc_ohc=rebANrhGkXAAX-OPG9o&amp;tp=1&amp;oh=4bcb9b3313dacdcfa0302f68636a4251&amp;oe=605EB32C" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fnlv2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c1.0.825.825a/s640x640/151387931_248131726986063_7146800644906673262_n.jpg?_nc_ht=instagram.fnlv2-1.fna.fbcdn.net&amp;_nc_cat=103&amp;_nc_ohc=p6zE9LI90KAAX__ecHq&amp;tp=1&amp;oh=d3d8fe9ca107c6dd6c51d31333101eb8&amp;oe=605EDCF2" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fnlv2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.20.866.866a/s640x640/150607721_1232644437130448_5432726904860811056_n.jpg?_nc_ht=instagram.fnlv2-1.fna.fbcdn.net&amp;_nc_cat=108&amp;_nc_ohc=LWkl5luZdusAX-e-dfC&amp;tp=1&amp;oh=1ebfe67ec5ea3c9e17eee399c5440317&amp;oe=60602578" alt="" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
