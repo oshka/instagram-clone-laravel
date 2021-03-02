e4fff4fdc6269a3981a0143620924ad5b25f5598
oshka <kikapo51>
2021-03-02 17:45:20 +0200

    [ADD] Showing Posts from Profiles The User Is Following

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+
+Route::get('/','PostsController@index');
 
 Route::post('follow/{user}', 'FollowsController@store');
 
